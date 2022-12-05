a20c07091767bcfc5172e4ae8f890db8d37cc6ec
cankinik <cankinik@gmail.com>
2022-12-05 17:07:08 -0500

    Commit before handling train

--- datasets/wrappers.py
+++ datasets/wrappers.py
@@ -9,8 +9,32 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 
 from datasets import register
-from utils import to_pixel_samples
-from utils import make_coord
+
+def make_coord(shape, ranges=None, flatten=True):
+    """ Make coordinates at grid centers.
+    """
+    coord_seqs = []
+    for i, n in enumerate(shape):
+        if ranges is None:
+            v0, v1 = -1, 1
+        else:
+            v0, v1 = ranges[i]
+        r = (v1 - v0) / (2 * n)
+        seq = v0 + r + (2 * r) * torch.arange(n).float()
+        coord_seqs.append(seq)
+    ret = torch.stack(torch.meshgrid(*coord_seqs), dim=-1)
+    if flatten:
+        ret = ret.view(-1, ret.shape[-1])
+    return ret
+
+
+def to_pixel_samples(img):
+    """ Convert the image to coord-RGB pairs.
+        img: Tensor, (3, H, W)
+    """
+    coord = make_coord(img.shape[-2:])
+    rgb = img.view(3, -1).permute(1, 0)
+    return coord, rgb
 
 @register('sr-implicit-paired')
 class SRImplicitPaired(Dataset):
--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .models import register, make
-from . import edsr, rdn, rcan, swinir
-from . import mlp
-
-from . import misc, liif, lte, ltep
